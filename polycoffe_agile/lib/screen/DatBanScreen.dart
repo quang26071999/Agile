984a9828ea1519fcea305f4966c6e86711d285b7
Phương Nam <namnpph18596@fpt.edu.vn>
2023-02-08 15:56:00 +0700

    DatBan Screen update UI

--- polycoffe_agile/lib/screen/DatBanScreen.dart
+++ polycoffe_agile/lib/screen/DatBanScreen.dart
@@ -8,7 +8,7 @@ class DatBanScreen extends StatefulWidget {
 }
 
 class _DatBanScreenState extends State<DatBanScreen> {
-  List TrangThai = [
+  List trangThai = [
     "Chưa thanh toán",
     "Trống",
     "Chưa thanh toán",
@@ -17,18 +17,22 @@ class _DatBanScreenState extends State<DatBanScreen> {
     "Trống",
   ];
 
-  List SoBan = [1, 2, 3, 4, 5, 6];
+  List soBan = [1, 2, 3, 4, 5, 6];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 25),
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.topRight,
-              child: Row(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/backgroundCoffee.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
                 children: const [
                   Text(
                     "Lọc theo",
@@ -40,78 +44,86 @@ class _DatBanScreenState extends State<DatBanScreen> {
                   Icon(
                     Icons.filter_alt_sharp,
                     size: 32,
-                  )
+                  ),
                 ],
               ),
-            ),
-            GridView.builder(
-                itemCount: TrangThai.length,
-                padding: const EdgeInsets.only(right: 20, left: 20, top: 68),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 23,
-                  crossAxisSpacing: 26,
-                ),
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) => Container(
-                      width: 147,
-                      height: 147,
-                      child: Card(
-                        color: const Color(0xffFFFFFF),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      style: const TextStyle(fontSize: 24),
-                                      "Bàn ${SoBan[index]}",
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 0),
-                                    child: Text(
-                                      TrangThai[index],
-                                      style: TextStyle(
-                                        color: TrangThai[index] ==
-                                                "Chưa thanh toán"
-                                            ? Color(0xffFF0000)
-                                            : Color(0xff0000FF),
-                                        fontSize: 16,
-                                        fontStyle: FontStyle.italic,
+              GridView.builder(
+                  itemCount: trangThai.length,
+                  padding: const EdgeInsets.only(right: 20, left: 20, top: 60),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 23,
+                    crossAxisSpacing: 26,
+                  ),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) => Container(
+                        width: 147,
+                        height: 147,
+                        child: Card(
+                          color: const Color(0xffFFFFFF),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 8.0),
+                                      child: Text(
+                                        style: const TextStyle(fontSize: 24),
+                                        "Bàn ${soBan[index]}",
                                       ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 21.0),
-                                    child: TrangThai[index] == "Chưa thanh toán"
-                                        ? Image.asset(
-                                            'assets/images/coffee.png',
-                                            width: 71,
-                                            height: 70,
-                                            alignment: Alignment.bottomCenter,
-                                          )
-                                        : Image.asset(
-                                            'assets/images/tableCoffee.png',
-                                            width: 70,
-                                            height: 70,
-                                            alignment: Alignment.bottomCenter,
-                                          ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 0),
+                                      child: Text(
+                                        trangThai[index],
+                                        style: TextStyle(
+                                          color: trangThai[index] ==
+                                                  "Chưa thanh toán"
+                                              ? Color(0xffFF0000)
+                                              : Color(0xff0000FF),
+                                          fontSize: 16,
+                                          fontStyle: FontStyle.italic,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding:
+                                            const EdgeInsets.only(top: 5.0),
+                                        child: trangThai[index] ==
+                                                "Chưa thanh toán"
+                                            ? Image.asset(
+                                                'assets/images/coffee.png',
+                                                width: 71,
+                                                height: 85,
+                                                alignment:
+                                                    Alignment.bottomCenter,
+                                              )
+                                            : Container(
+                                                padding: const EdgeInsets.only(
+                                                    top: 16),
+                                                child: Image.asset(
+                                                  'assets/images/tableCoffee.png',
+                                                  width: 70,
+                                                  height: 70,
+                                                  alignment:
+                                                      Alignment.bottomCenter,
+                                                ),
+                                              ))
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    )),
-          ],
+                      )),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
