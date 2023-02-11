b7c393247645c858992a41a7d80ec4d1f1b5c19d
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2023-02-11 21:47:49 +0700

    EditScreen

--- polycoffe_agile/lib/constants.dart
+++ polycoffe_agile/lib/constants.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 const defaultPadding = 20.0;
-
+const color = Color.fromRGBO(222, 205, 185, 1);
 const defaultDuration = Duration(milliseconds: 400);
+class GioiTinhChoice {
+  String gioiTinhChoice;
+  int indexGT;
+  GioiTinhChoice({required this.indexGT, required this.gioiTinhChoice});
+}
+String default_gioiTinh = "Nam";
+int default_index_gioiTinh = 1;
+List<GioiTinhChoice> listGT = [
+  GioiTinhChoice(indexGT: 0, gioiTinhChoice: "Nam"),
+  GioiTinhChoice(indexGT: 1, gioiTinhChoice: "Nữ"),
+];
+
