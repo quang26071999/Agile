a7103d167461af5e28c4aa39f341c1a6729ff6ea
Administrator <mr73367@gmail.com>
2023-02-19 20:03:35 +0700

    der

--- polycoffe_agile/lib/screen/DatBanScreen.dart
+++ polycoffe_agile/lib/screen/DatBanScreen.dart
@@ -67,120 +67,121 @@ class _DatBanScreenState extends State<DatBanScreen> {
                   ),
                 ),
               ),
-              StreamBuilder(
-                stream:
-                    FirebaseFirestore.instance.collection("Table").snapshots(),
-                builder: (context, snapShot) {
-                  if (snapShot.hasData) {
-                    maBan =
-                        "B0${(snapShot.data?.docs.length)}${DateTime.now().millisecondsSinceEpoch}";
-                    return GridView.builder(
-                        itemCount: snapShot.data?.docs.length,
-                        padding:
-                            const EdgeInsets.only(right: 20, left: 20, top: 60),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 23,
-                          crossAxisSpacing: 26,
-                        ),
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          DocumentSnapshot docSnap = snapShot.data?.docs[index]
-                              as DocumentSnapshot<Object?>;
-                          String id = docSnap.id;
-                          if (docSnap["HDT"] != null) {
-                            colTable
-                                .doc(id)
-                                .update({"trangThai": "Chưa Thanh Toán"});
-                          } else {
-                            colTable.doc(id).update({"trangThai": "Trống"});
-                          }
-                          String trangThai = docSnap["trangThai"];
-                          return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => MenuOrder(
-                                              numTable: "Bàn ${index + 1}",
-                                              idBan: id,
-                                            )));
-                              },
-                              child: SizedBox(
-                                width: 147,
-                                height: 147,
-                                child: Card(
-                                  color: const Color(0xffFFFFFF),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30.0),
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 8),
-                                        child: Column(
-                                          children: [
-                                            Padding(
-                                              padding: const EdgeInsets.only(
-                                                  bottom: 8.0),
-                                              child: Text(
-                                                style: GoogleFonts.inter(
-                                                    fontSize: 24),
-                                                "Bàn ${index + 1}",
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.only(
-                                                  bottom: 0),
-                                              child: Text(
-                                                trangThai,
-                                                style: GoogleFonts.inter(
-                                                  color: trangThai ==
-                                                          "Chưa Thanh Toán"
-                                                      ? const Color(0xffFF0000)
-                                                      : const Color(0xff0000FF),
-                                                  fontSize: 16,
-                                                  fontStyle: FontStyle.italic,
+              Expanded(
+                child: StreamBuilder(
+                  stream:
+                      FirebaseFirestore.instance.collection("Table").snapshots(),
+                  builder: (context, snapShot) {
+                    if (snapShot.hasData) {
+                      maBan =
+                          "B0${(snapShot.data?.docs.length)}${DateTime.now().millisecondsSinceEpoch}";
+                      return GridView.builder(
+                          itemCount: snapShot.data?.docs.length,
+                          padding:
+                              const EdgeInsets.only(right: 20, left: 20, top: 20),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 23,
+                            crossAxisSpacing: 26,
+                          ),
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            DocumentSnapshot docSnap = snapShot.data?.docs[index]
+                                as DocumentSnapshot<Object?>;
+                            String id = docSnap.id;
+                            if (docSnap["HDT"] != null) {
+                              colTable
+                                  .doc(id)
+                                  .update({"trangThai": "Chưa Thanh Toán"});
+                            } else {
+                              colTable.doc(id).update({"trangThai": "Trống"});
+                            }
+                            String trangThai = docSnap["trangThai"];
+                            return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MenuOrder(
+                                                numTable: "Bàn ${index + 1}",
+                                                idBan: id,
+                                              )));
+                                },
+                                child: SizedBox(
+                                  width: 147,
+                                  height: 147,
+                                  child: Card(
+                                    color: const Color(0xffFFFFFF),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 8),
+                                          child: Column(
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    bottom: 8.0),
+                                                child: Text(
+                                                  style: GoogleFonts.inter(
+                                                      fontSize: 24),
+                                                  "Bàn ${index + 1}",
                                                 ),
                                               ),
-                                            ),
-                                            Padding(
+                                              Padding(
                                                 padding: const EdgeInsets.only(
-                                                    top: 5.0),
-                                                child: trangThai ==
-                                                        "Chưa Thanh Toán"
-                                                    ? Image.asset(
-                                                        'assets/images/coffee.png',
-                                                        width: 71,
-                                                        height: 85,
-                                                        alignment: Alignment
-                                                            .bottomCenter,
-                                                      )
-                                                    : Container(
-                                                        padding:
-                                                            const EdgeInsets
-                                                                .only(top: 16),
+                                                    bottom: 0),
+                                                child: Text(
+                                                  trangThai,
+                                                  style: GoogleFonts.inter(
+                                                    color: trangThai ==
+                                                            "Chưa Thanh Toán"
+                                                        ? const Color(0xffFF0000)
+                                                        : const Color(0xff0000FF),
+                                                    fontSize: 16,
+                                                    fontStyle: FontStyle.italic,
+                                                  ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                  padding: const EdgeInsets.only(
+                                                      top: 5.0),
+                                                  child: trangThai ==
+                                                          "Chưa Thanh Toán"
+                                                      ? Container(
                                                         child: Image.asset(
-                                                          'assets/images/tableCoffee.png',
-                                                          width: 70,
-                                                          height: 70,
-                                                          alignment: Alignment
-                                                              .bottomCenter,
-                                                        ),
-                                                      ))
-                                          ],
-                                        ),
-                                      )
-                                    ],
+                                                            'assets/images/coffee.png',
+                                                            width: 70,
+                                                            height: 70,
+                                                            alignment: Alignment
+                                                                .bottomCenter,
+                                                          ),
+                                                      )
+                                                      : Container(
+                                                          child: Image.asset(
+                                                            'assets/images/tableCoffee.png',
+                                                            width: 70,
+                                                            height: 70,
+                                                            alignment: Alignment
+                                                                .bottomCenter,
+                                                          ),
+                                                        ))
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ));
-                        });
-                  } else {
-                    return const Text("Không có bàn nào");
-                  }
-                },
+                                ));
+                          });
+                    } else {
+                      return const Text("Không có bàn nào");
+                    }
+                  },
+                ),
               )
             ],
           ),
