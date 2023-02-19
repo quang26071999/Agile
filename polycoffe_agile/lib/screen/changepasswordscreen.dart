f9654cd896d7b4e826fa05de274a1477017269c2
thehung2001 <hungnt201x@gmail.com>
2023-02-19 16:55:08 +0700

    Update changepasswordscreen.dart

--- polycoffe_agile/lib/screen/changepasswordscreen.dart
+++ polycoffe_agile/lib/screen/changepasswordscreen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -39,8 +38,8 @@ class _ChangePass extends State<ChangePass> {
   Future<void> initial() async {
     logindata = await SharedPreferences.getInstance();
     setState(() {
-      user = logindata.getString("username")!;
-      password = logindata.getString("password")!;
+      user = logindata!.getString("username");
+      password = logindata!.getString("password");
     });
   }
 
@@ -49,7 +48,8 @@ class _ChangePass extends State<ChangePass> {
     return Container(
         decoration: BoxDecoration(color: Color(0xffAE8E6F)),
         child: SafeArea(
-          child: Container(child: Column(
+            child: Container(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("Thay đổi mật khẩu",
@@ -59,121 +59,168 @@ class _ChangePass extends State<ChangePass> {
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       decoration: TextDecoration.none)),
-              Padding(padding: EdgeInsets.only(left: 36, right: 36),child: Form(
-                  key: _formKey,
-                  child: SingleChildScrollView(child: Column(
-                    children: [
-                      Padding(padding: EdgeInsets.only(top: 56, bottom: 24),child: TextFormField(
-                        controller: _oldPass,
-                        style: GoogleFonts.inter(
-                            fontSize: 16, fontStyle: FontStyle.italic),
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: const Color(0xffDECDB9),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 15),
-                            hintText: "Nhập mật khẩu cũ",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide: const BorderSide(),
-                            )),
-                        validator: (text) {
-                          if (text == null ||
-                              text.isEmpty) {
-                            return 'Vui lòng nhập mật khẩu cũ';
-                          }
-                          if(text != password.toString()){
-                            return 'Mật khẩu cũ chưa chính xác';
-                          }
-                          return null;
-                        },
-                      ),),
-                      Padding(padding: EdgeInsets.only(bottom: 24),child: TextFormField(
-                        controller: _newPass,
-                        style: GoogleFonts.inter(
-                            fontSize: 16, fontStyle: FontStyle.italic),
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: const Color(0xffDECDB9),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 15),
-                            hintText: "Nhập mật khẩu mới",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide: const BorderSide(),
-                            )),
-                        validator: (text) {
-                          if (text == null ||
-                              text.isEmpty) {
-                            return 'Vui lòng nhập mật khảu mới';
-                          }
-                          return null;
-                        },
-                      ),),
-                      Padding(padding: EdgeInsets.only(bottom: 40),child: TextFormField(
-                        controller: _reNewPass,
-                        style: GoogleFonts.inter(
-                            fontSize: 16, fontStyle: FontStyle.italic),
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: const Color(0xffDECDB9),
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 15),
-                            hintText: "Nhập lại mật khẩu mới",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide: const BorderSide(),
-                            )),
-                        validator: (text) {
-                          if (text == null ||
-                              text.isEmpty) {
-                            return "Vui lòng nhập lại mật khảu mới";
-                          }
-                          if(text != _newPass.text){
-                            return "Mật khẩu mới nhập lại không khớp";
-                          }
-                          return null;
-                        },
-                      ),),
-
-                      Container(
-                        width: 288,
-                        child: TextButton(
-                          onPressed: () {
-                            // _formKey.currentState!.validate();
-                            print('USER ${user}');
-                            if(_formKey.currentState!.validate()){
-                              var documentReference = FirebaseFirestore.instance.collection("User").doc(user);
-                              documentReference.update({
-                                "password": _newPass.text
-                              })
-                                  .then((value) =>
-                                  // debugPrint("Sửa thành công")
-                                Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context) => TaiKhoanScreen()))
-                              )
-                                  .catchError((error) => debugPrint("Sửa thất bại:  ${error}"));
-
-
-                            }else{
-                              print("false");
-                            }
-                          },
-                          style: TextButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor: Colors.brown[900],
+              Padding(
+                padding: EdgeInsets.only(left: 36, right: 36),
+                child: Form(
+                    key: _formKey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 56, bottom: 24),
+                            child: TextFormField(
+                              controller: _oldPass,
+                              style: GoogleFonts.inter(
+                                  fontSize: 16, fontStyle: FontStyle.italic),
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: const Color(0xffDECDB9),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 15),
+                                  hintText: "Nhập mật khẩu cũ",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(),
+                                  )),
+                              validator: (text) {
+                                if (text == null || text.isEmpty) {
+                                  return 'Vui lòng nhập mật khẩu cũ';
+                                }
+                                if (text != password.toString()) {
+                                  return 'Mật khẩu cũ chưa chính xác';
+                                }
+                                return null;
+                              },
+                            ),
                           ),
-                          child: Text(
-                            "Thay đổi",
-                            style: GoogleFonts.inter(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.w700),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 24),
+                            child: TextFormField(
+                              controller: _newPass,
+                              style: GoogleFonts.inter(
+                                  fontSize: 16, fontStyle: FontStyle.italic),
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: const Color(0xffDECDB9),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 15),
+                                  hintText: "Nhập mật khẩu mới",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(),
+                                  )),
+                              validator: (text) {
+                                if (text == null || text.isEmpty) {
+                                  return 'Vui lòng nhập mật khảu mới';
+                                }
+                                if (text.length < 5) {
+                                  return "Vui lòng nhập khẩu mới trên 5 kí tự";
+                                }
+                                return null;
+                              },
+                            ),
                           ),
-                        ),)
-                    ],
-                  ),)),)
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 40),
+                            child: TextFormField(
+                              controller: _reNewPass,
+                              style: GoogleFonts.inter(
+                                  fontSize: 16, fontStyle: FontStyle.italic),
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: const Color(0xffDECDB9),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 15),
+                                  hintText: "Nhập lại mật khẩu mới",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(),
+                                  )),
+                              validator: (text) {
+                                if (text == null || text.isEmpty) {
+                                  return "Vui lòng nhập lại mật khảu mới";
+                                }
+                                if (text != _newPass.text) {
+                                  return "Mật khẩu mới nhập lại không khớp";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: 288,
+                            child: TextButton(
+                              onPressed: () {
+                                // print('USER ${user}');
+                                if (_formKey.currentState!.validate()) {
+                                  var documentReference = FirebaseFirestore
+                                      .instance
+                                      .collection("User")
+                                      .doc(user);
+                                  documentReference
+                                      .update({"password": _newPass.text})
+                                      .then((value) =>
+                                          debugPrint("Sửa thành công"))
+                                      .catchError((error) => debugPrint(
+                                          "Sửa thất bại:  ${error}"));
+                                  setState(() {
+                                    showDialog<String>(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                            insetPadding: EdgeInsets.zero,
+                                            title: const Text(
+                                              'Thông báo',
+                                              textAlign: TextAlign.center,
+                                            ),
+                                            content: Text(
+                                                'Thay đổi mật khẩu thành công'),
+                                            actions: <Widget>[
+                                              TextButton(
+                                                onPressed: () {
+                                                  logindata!.setString("password", _newPass.text);
+                                                  password = logindata!.getString("password");
+                                                  setEmpty();
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text(
+                                                  'OK', //title
+                                                  textAlign: TextAlign.end,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                    );
+                                  });
+                                }
+                                // else {
+                                //   print("false");
+                                // }
+                              },
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: Colors.brown[900],
+                              ),
+                              child: Text(
+                                "Thay đổi",
+                                style: GoogleFonts.inter(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              )
             ],
-          ),)
-        ));
+          ),
+        )));
+  }
+  void setEmpty()  {
+    _oldPass.text = "";
+    _newPass.text = "";
+    _reNewPass.text = "";
   }
 }
