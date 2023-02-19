7d88f6a659acc061dae9b975bb27497ba9b4d260
HienBull98 <phunghien9812@gmail.com>
2023-02-19 16:07:17 +0700

    validate DangNhapScreen

--- polycoffe_agile/lib/screen/DangNhapScreen.dart
+++ polycoffe_agile/lib/screen/DangNhapScreen.dart
@@ -6,6 +6,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:polycoffe_agile/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../models/user.dart';
+
 class DangNhapScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -28,6 +30,7 @@ class _MyLoginState extends State<MyLogin>{
   late SharedPreferences logindata;
   late bool newuser;
   late bool _passVisible;
+    final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -55,151 +58,178 @@ class _MyLoginState extends State<MyLogin>{
 
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background5.jpg"),
-            fit: BoxFit.cover,
-          ),
+          color: Color(0xffAE8E6F),
         ),
         // padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
         constraints: BoxConstraints.expand(),
         child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                // height: 140,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(61, 261, 50, 0),
-                child: Text(
-                  "POLY COFFEE",
-                    style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                            fontSize: 36,
-                            color: Colors.black,
-                            fontWeight:
-                            FontWeight.w900))
-                  // fontFamily: Inter,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  // height: 140,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(36, 30, 36, 35),
-                child: TextField(
-                  controller: usernameController,
-                  style:  GoogleFonts.inter(fontSize: 20,color: Colors.black,),
-                  decoration: InputDecoration(
-                      labelText: "Username",
-                      filled: true,
-                      fillColor:   Color(0xffDECDB9),
-                      prefixIcon: Container(
-                          width: 50, child: Icon(Icons.person)),
-                      border: const OutlineInputBorder(
-                          borderSide:
-                          BorderSide(color: Colors.black,width: 2),
-                          borderRadius: BorderRadius.all(Radius.circular(20)))),
+                Container(
+                  width: 300,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/cafe.png'),
+                      )
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(36, 0, 36, 8),
-                child: Stack(
-                  alignment: AlignmentDirectional.centerEnd,
-                  children: <Widget>[
-                    TextField(
-                      controller: passwordController,
-                      style:  GoogleFonts.inter(fontSize: 20,color: Colors.black),
-                      obscureText: !_passVisible,
-                      decoration: InputDecoration(
-                          labelText: "Password",
-                          filled: true,
-                          fillColor:   Color(0xffDECDB9),
-                          prefixIcon: Container(
-                              width: 50, child: Icon(Icons.key)),
-                          border: const OutlineInputBorder(
-                              borderSide:
-                              BorderSide(color: Colors.black,width: 2),
-                              borderRadius: BorderRadius.all(Radius.circular(20)))),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(61, 0, 50, 0),
+                  child: Text(
+                    "POLY COFFEE",
+                      style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                              fontSize: 36,
+                              color: Colors.black,
+                              fontWeight:
+                              FontWeight.w900))
+                    // fontFamily: Inter,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(36, 30, 36, 35),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: usernameController,
+                    style:  GoogleFonts.inter(fontSize: 20,color: Colors.black,),
+                    decoration: InputDecoration(
+                        labelText: "Username",
+                        filled: true,
+                        fillColor:   Color(0xffDECDB9),
+                        prefixIcon: Container(
+                            width: 50, child: Icon(Icons.person)),
+                        border: const OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.black,width: 2),
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                        )
                     ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(270,4,0,0),
-                      child:  IconButton(
-                          icon: Icon(
-                            _passVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: Theme.of(context).primaryColorDark,
-                          ),
-                          onPressed: (){
-                            setState(() {
-                              _passVisible = !_passVisible;
-                            });
-                          }),
-
-                    )
-
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 0, 36, 50),
-                child: CheckboxListTile(
-                  title: const Text('Nhớ mật khẩu',
-                    style: TextStyle(fontSize: 20,color: Colors.black),),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: true,
-                  onChanged:(bool? value) { },
-                ),
-              ),
-
-              // suffixIcon
-              Padding(
-                padding: const EdgeInsets.fromLTRB(36, 0, 36, 0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 52,
-                  child: TextButton(
-                    onPressed: () {
-                      final userRef = FirebaseFirestore.instance.collection("User");
-                      userRef.get().then((QuerySnapshot querySnapshot){
-                        querySnapshot.docs.forEach((doc) async {
-                          if(usernameController.text == doc["username"] && passwordController.text == doc["password"]){
-                           await logindata.setBool("login", false);
-                           await logindata.setString("username", usernameController.text);
-                           await logindata.setString("name", doc['hoTen']);
-                           await logindata.setString("dateOfBirth", doc['ngaySinh']);
-                           await logindata.setString("sex", doc['gioiTinh']);
-                           await logindata.setString("avatar", doc['avatar']);
-                           await logindata.setString("role", doc['role']);
-                           await logindata.setString("address", doc['diaChi']);
-                           await logindata.setString("password", doc['password']);
-                            Get.snackbar("Successfully", "Dang nhap thanh cong");
-                            Get.off(MyApp());
-                          }else if(usernameController.text == "admin" && passwordController.text == "admin"){
-                            logindata.setBool("login", false);
-                            logindata.setString("role", 'admin');
-                            logindata.setString("username", usernameController.text);
-
-                            Get.off(MyApp());
-                            Get.snackbar("Successfully", "Dang nhap thanh cong");
-                          }
-                          else{
-                            Get.snackbar("Error", "sai thong tin dang nhap");
-                          }
-                        });
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Vui lòng không để trống";
                       }
-                      );
+                      if(!RegExp(r'^(84|0)+([0-9]{9})+$').hasMatch(value!)){
+                        return "Số điện thoại không đúng định dạng";
+                      }
+                      else{
+                        return null;
+                      }
                     },
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      backgroundColor: Colors.brown[900],
-                    ),
-                    child: Text(
-                      "Đăng nhập",
-                      style: GoogleFonts.inter(color: Colors.white,fontSize: 30,fontWeight: FontWeight.w700),
+
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(36, 0, 36, 8),
+                  child: Stack(
+                    alignment: AlignmentDirectional.centerEnd,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: passwordController,
+                        style:  GoogleFonts.inter(fontSize: 20,color: Colors.black),
+                        obscureText: !_passVisible,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            filled: true,
+                            fillColor:   Color(0xffDECDB9),
+                            prefixIcon: Container(
+                                width: 50, child: Icon(Icons.key)),
+                            border: const OutlineInputBorder(
+                                borderSide:
+                                BorderSide(color: Colors.black,width: 2),
+                                borderRadius: BorderRadius.all(Radius.circular(20)))),
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "Vui lòng không để trống";
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(270,4,0,0),
+                        child:  IconButton(
+                            icon: Icon(
+                              _passVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                _passVisible = !_passVisible;
+                              });
+                            }),
+
+                      )
+
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 36, 50),
+                  child: CheckboxListTile(
+                    title: const Text('Nhớ mật khẩu',
+                      style: TextStyle(fontSize: 20,color: Colors.black),),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: true,
+                    onChanged:(bool? value) { },
+                  ),
+                ),
+
+                // suffixIcon
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(36, 0, 36, 0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 52,
+                    child: TextButton(
+                      onPressed: () {
+                        if(_formKey.currentState!.validate()){
+                          final userRef = FirebaseFirestore.instance.collection("User").doc(usernameController.text);
+                          userRef.get().then((doc)  async {
+                            if(usernameController.text == doc["username"] && passwordController.text == doc["password"]){
+                              await logindata.setBool("login", false);
+                              await logindata.setString("username", usernameController.text);
+                              await logindata.setString("name", doc['hoTen']);
+                              await logindata.setString("dateOfBirth", doc['ngaySinh']);
+                              await logindata.setString("sex", doc['gioiTinh']);
+                              await logindata.setString("avatar", doc['avatar']);
+                              await logindata.setString("role", doc['role']);
+                              await logindata.setString("address", doc['diaChi']);
+                              await logindata.setString("password", doc['password']);
+
+                              //final User user = User(doc['username'], doc['password'], doc['hoTen'], doc['role'], doc['gioiTinh'], doc['diaChi'], doc['avatar'], doc['ngaySinh']);
+                              Get.snackbar("Successfully", "Dang nhap thanh cong");
+                              Get.off(() => MyApp());
+
+                            }
+                            else{
+                              Get.snackbar("Error", "Sai thong tin dang nhap");
+                            }
+
+                          }
+                          );
+                        }
+                      },
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.brown[900],
+                      ),
+                      child: Text(
+                        "Đăng nhập",
+                        style: GoogleFonts.inter(color: Colors.white,fontSize: 30,fontWeight: FontWeight.w700),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-            ],
+              ],
+            ),
           ),
         ),
 
